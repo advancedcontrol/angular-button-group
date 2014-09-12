b47ac427c5240629ffd1fc6af0ee5ad14e8a607c
Stephen von Takach <steve@cotag.me>
2014-09-12 17:15:01 +1000

    init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Angular Button Group
+
+Effectivley an alternative to standard HTML Radio buttons.
+
+
+![screen shot of demo](https://cloud.githubusercontent.com/assets/368013/4246538/607244ea-3a4c-11e4-94b9-8d579bddcd11.png)
+
+
+## Installation
+
+1. Open bower.json
+2. Add `"angular-button-group": "latest"` to your dependency list
+3. Run `bower install`
+4. In your application you can now add:
+   * `<script src="bower_components/angular-button-group/button-group.js"></script>`
+
+
+## Usage
+
+The demo uses the imported SCSS styles. These were extracted from bootstrap for demonstration purposes. You may use bootstrap styles or create your own.
+
+
+See the demo file
---
+++ button-group.js
@@ -0,0 +1,75 @@
+(function (angular) {
+    'use strict';
+
+
+    // Mix in to our module
+    var module;
+    try {
+        module = angular.module('coUtils');
+    } catch (e) {
+        module = angular.module('coUtils', []);
+    }
+
+
+    module
+        .directive('btnGroup', function() {
+            return {
+                restrict: 'A',
+                controller: ['$scope', '$element', function($scope, $element) {
+                    var buttons = [],
+                        btnClass = $element.attr('btn-class') || 'btn',
+                        addClass = true;
+
+                    if (btnClass === 'none') {
+                        addClass = false;
+                    } else {
+                        $element.addClass(btnClass + '-group');
+                    }
+
+                    this.addBtn = function(btn) {
+                        if (addClass) {
+                            btn.element.addClass(btnClass);
+                        }
+                        buttons.push(btn);
+                    }
+
+                    this.removeBtn = function(btn) {
+                        var index = buttons.indexOf(btn);
+                        if (index > -1) {
+                            buttons.splice(index, 1);
+                        }
+                    }
+
+                    $scope.$watch($element.attr('btn-group'), function (value) {
+                        angular.forEach(buttons, function (btn) {
+                            if (value === $scope.$eval(btn.value)) {
+                                btn.element.addClass('active');
+                            } else {
+                                btn.element.removeClass('active');
+                            };
+                        });
+                    });
+                }]
+            };
+        })
+
+        .directive('btn', function() {
+            return {
+                require: '^btnGroup',
+                restrict: 'A',
+                link: function(scope, element, attrs, groupCtrl) {
+                    var btnRef = {
+                        value: attrs.btn,
+                        element: element
+                    };
+
+                    groupCtrl.addBtn(btnRef);
+
+                    scope.$on('$destroy', function () {
+                        groupCtrl.removeBtn(btnRef);
+                    });
+                }
+            };
+        });
+
+}(this.angular));
